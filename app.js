4ca271bff7d48765a5631a819d929a74d4561c99
Stephanie Adams <adamsstephaniem@gmail.com>
2017-02-26 11:02:44 -0500

    refactor port

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('views', __dirname + '/views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static('browser'));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
     res.render('index', { 'rows': [['','',''],['','',''],['','','']], 'nums': Array(9).fill(0) } );
@@ -18,7 +19,7 @@ app.use(function(req, res){
     res.sendStatus(404);
 });
 
-var server = app.listen(5000, function() {
-    var port = server.address().port;
+var server = app.listen(app.get('port'), function() {
+    var port = app.get('port');
     console.log('Server listening on port %s.', port);
 });
